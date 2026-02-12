a458ff2273f41ac400ea17de87e708125b65226f
Simei <75575212+Simei-Alves@users.noreply.github.com>
2026-02-12 11:53:33 +0000

    Update minecraft.js

--- minecraft.js
+++ minecraft.js
@@ -1,30 +1,25 @@
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-<meta charset="UTF-8">
-<title>Minecraft Classic</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+(function() {
 
-<body style="margin:0;font-family:Arial, sans-serif;">
+  var container = document.createElement("div");
+  container.style.maxWidth = "960px";
+  container.style.margin = "40px auto";
 
-<div style="max-width:960px;margin:40px auto;">
-
-    <iframe
-        src="https://classic.minecraft.net/"
-        width="100%"
-        height="600"
-        frameborder="0"
-        allowfullscreen>
+  container.innerHTML = `
+    <iframe 
+      src="https://classic.minecraft.net/" 
+      width="100%" 
+      height="600" 
+      frameborder="0" 
+      allowfullscreen>
     </iframe>
 
     <p style="text-align:center;margin-top:10px;">
-        <a href="https://classic.minecraft.net/" target="_blank">
-            Play Minecraft Classic
-        </a>
+      <a href="https://classic.minecraft.net/" target="_blank">
+        Play Minecraft Classic
+      </a>
     </p>
+  `;
 
-</div>
+  document.currentScript.parentNode.insertBefore(container, document.currentScript);
 
-</body>
-</html>
+})();
